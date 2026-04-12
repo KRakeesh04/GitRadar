82f0fe916ca09edc8a7f4cca95016fc7a016ddec
KRakeesh04 <kanesrakeshan@gmail.com>
2026-04-13 03:45:06 +0530

    rename rust-fmt & run just-fmt

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,9 @@ pub fn run() {
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
-                    tauri_plugin_log::Builder::default().level(log::LevelFilter::Info).build(),
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
                 )?;
             }
             Ok(())
